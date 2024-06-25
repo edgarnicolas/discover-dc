6c2945c3c25096d3834873985989ea3e5d4b7869
edgarnicolas <guzman.edgar@aol.com>
2024-06-25 12:55:06 -0400

    created discover debit card ad

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    gsap.to(".button-glare", {
+        duration: 0.20,  
+        x: '150%',
+        ease: "power1.inOut",
+    });
+
+});
