51586abd8fb1d4a40546a4ece82cfaeb24fa8132
edgarnicolas <guzman.edgar@aol.com>
2024-07-20 14:40:21 -0400

    added animated effect to the card

--- js/script.js
+++ js/script.js
@@ -5,4 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
         ease: "power1.inOut",
     });
 
+    gsap.to(".debit-card", {
+        duration: 0.5,  
+        y: '-300px',  
+        ease: "power2.out",
+        delay: 0.50, 
+        onComplete: function() {
+            gsap.to(".debit-card", {
+                duration: 0.3,
+                scale: 1.1, // Scale up the card a bit
+                ease: "power1.inOut",
+                onComplete: function() {
+                    gsap.to(".debit-card", {
+                        duration: 0.5,
+                        scale: 1.0, // Return to normal size with a bounce effect
+                        ease: "bounce.out",
+                    });
+                }
+            });
+        }
+    });
+
 });
