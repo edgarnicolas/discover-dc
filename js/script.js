f7f4bd291fd432de68a489134261ee3e27a088bd
edgarnicolas <guzman.edgar@aol.com>
2024-07-21 13:17:34 -0400

    added text fade transition

--- js/script.js
+++ js/script.js
@@ -1,42 +1,55 @@
 document.addEventListener("DOMContentLoaded", function() {
-    gsap.timeline()
-        .to(".arcs", {
-            duration: 1,
-            scale: 1,
-            ease: "power2.inOut"
-        })
-        .to(".disappearing-circle", {
-            duration: 1,
-            opacity: 0,
-            ease: "power2.inOut",
-            onComplete: function() {
-                gsap.to(".button-glare", {
-                    duration: 0.20,
-                    x: '150%',
-                    ease: "power1.inOut",
-                });
-                gsap.to(".debit-card", {
-                    duration: 1,  
-                    bottom: '0px', 
-                    right: '0px', 
-                    rotation: 0,  
-                    ease: "power2.out",
-                    delay: 0.01, 
-                    onComplete: function() {
-                        gsap.to(".debit-card", {
-                            duration: 0.1,
-                            scale: 1.01, // Scale up the card a bit
-                            ease: "power1.inOut",
-                            onComplete: function() {
-                                gsap.to(".debit-card", {
-                                    duration: 0.5,
-                                    scale: 1.0, // Return to normal size with a bounce effect
-                                    ease: "bounce.out",
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        }, "-=1"); // Start this animation slightly earlier to overlap with the scale animation
+    const tl = gsap.timeline();
+
+    tl.to(".arcs", {
+        duration: 1,
+        scale: 1,
+        ease: "power2.inOut"
+    })
+    .to(".disappearing-circle", {
+        duration: 1,
+        opacity: 0,
+        ease: "power2.inOut",
+        onComplete: function() {
+            gsap.to(".button-glare", {
+                duration: 0.20,
+                x: '150%',
+                ease: "power1.inOut",
+            });
+            gsap.to(".debit-card", {
+                duration: 0.5,  
+                bottom: '0px', 
+                right: '0px', 
+                rotation: 0,  
+                ease: "power2.out",
+                delay: 0.01, 
+                onComplete: function() {
+                    gsap.to(".debit-card", {
+                        duration: 0.1,
+                        scale: 1.01, 
+                        ease: "power1.inOut",
+                        onComplete: function() {
+                            gsap.to(".debit-card", {
+                                duration: 0.5,
+                                scale: 1.0, 
+                                ease: "bounce.out",
+                            });
+                        }
+                    });
+                }
+            });
+
+            gsap.fromTo(".ad-content", {
+                opacity: 0,
+                x: 100
+            }, {
+                opacity: 1,
+                x: 0,
+                duration: 1,
+                ease: "power2.out"
+            });
+        }
+    }, "-=1");
+
+    
 });
